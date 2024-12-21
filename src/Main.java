db447b83577d604978cc1133f4880bec7a3f902a
Akylbek-prog <akylbekakmyrzaev@gmail.com>
2024-12-21 18:26:30 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Hero[] heroes = {
+                new Magic(100, 20, "FIREBALL"),
+                new Medic(80, 10, "HEALING", 50),
+                new Warrior(120, 30, "CRITICAL DAMAGE")
+        };
+
+        for (Hero hero : heroes) {
+            hero.applySuperAbility();
+            if (hero instanceof Medic) {
+                Medic medic = (Medic) hero;
+                medic.increaseExperience();
+            }
+        }
+    }
+}
---
+++ src/Medic.java
@@ -0,0 +1,26 @@
+public class Medic extends Hero {
+    private int healPoints;
+
+    public Medic(int health, int damage, String superAbilityType, int healPoints) {
+        super(health, damage, superAbilityType);
+        this.healPoints = healPoints;
+    }
+
+    public int getHealPoints() {
+        return healPoints;
+    }
+
+    public void setHealPoints(int healPoints) {
+        this.healPoints = healPoints;
+    }
+
+    public void increaseExperience() {
+        healPoints += healPoints * 0.1;
+        System.out.println("Medic увеличил опыт лечения. Текущие healPoints: " + healPoints);
+    }
+
+    @Override
+    public void applySuperAbility() {
+        System.out.println("Medic применил супер способность " + getSuperAbilityType());
+    }
+}
